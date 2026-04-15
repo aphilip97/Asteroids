8101c0d42a437f66672bff944dc8e1cd21589e6d
Athul Philip <athul@bytecafe.org>
2026-04-15 18:59:26 +0100

    Completed Lesson 4: FPS from Chapter 2: Gameloop

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode( size=( SCREEN_WIDTH, SCREEN_HEIGHT ) )
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
 
         log_state()
@@ -19,8 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
+        screen.fill( "black" )
         pygame.display.flip()
+
+        dt = clock.tick( 60 ) / 1000
 
 if __name__ == "__main__":
     main()
