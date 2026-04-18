e109d3535bcc0461a86408de061633bab36968eb
Athul Philip <athul@bytecafe.org>
2026-04-18 13:46:12 +0100

    Completed Lesson 1: Asteroids from Chapter 4: Asteroids

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CENTRE_X, CENTRE_Y
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 
@@ -15,10 +17,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player( x = CENTRE_X, y = CENTRE_Y )
+    field = AsteroidField()
 
     clock = pygame.time.Clock()
     dt = 0
